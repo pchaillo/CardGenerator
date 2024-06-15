58967f7568e34c75ec145fd5731391727e390a5a
pchaillo <paul.chaillou@inria.fr>
2024-06-15 18:37:19 +0200

    carte OK, comment les générer toutes ?

--- png_to_card.py
+++ png_to_card.py
@@ -3,6 +3,30 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import numpy as np
 
+def add_type(draw, nb_str,font):
+	if nb_str[0] == '6':
+		print("dragon")
+		txt = "DRAGON"
+		_, _, w, h = draw.textbbox((0, 0), txt, font=font)
+	draw.text(((1000-w)/2, 20),txt,(0,0,0),font=font)
+
+def add_classic_symbol( classic_nb,font,img,classic_pos,symbol_size,color = "pic"):
+	if color == "pic":
+		symbol_img = Image.open("./symbol/pic.png")
+	elif color == "carreau":
+		symbol_img = Image.open("./symbol/carreau.png")
+	elif color == "coeur":
+		symbol_img = Image.open("./symbol/coeur.png")
+	elif color == "trefle":
+		symbol_img = Image.open("./symbol/trefle.png")
+	symbol_img = symbol_img.resize((symbol_size, symbol_size), Image.LANCZOS)
+	symbol_img = symbol_img.convert("RGBA")
+	img = img.convert("RGBA")
+	img.alpha_composite(symbol_img, dest=(60, classic_pos))
+	draw = ImageDraw.Draw(img)	
+	draw.text((20 , classic_pos),str(classic_nb),(0,0,0),font=font) # numéro carte classique
+
+	return img
 
 def create_card(png_file,font_file,number): # Créer un classe carte
 
@@ -28,15 +52,17 @@ def create_card(png_file,font_file,number): # Créer un classe carte
 	draw = ImageDraw.Draw(img)	# Ajout du nombre
 	draw.text((20, 20),nb_str[0],(0,0,0),font=myBigFont)
 	draw.text((20 + BigFontSize/2 +5 , 20),nb_str[1],(0,0,0),font=myFont)
-	draw.text((20 , classic_pos),str(classic_nb),(0,0,0),font=classicFont) # numéro carte classique
 
-	symbol_img = Image.open("./symbol/pic.png")
-	symbol_img = symbol_img.resize((symbol_size, symbol_size), Image.LANCZOS)
+	add_type(draw, nb_str,myFont)
+	img = add_classic_symbol(classic_nb=classic_nb,img=img,classic_pos=classic_pos,color = "pic",font = classicFont,symbol_size=symbol_size)
 
-	symbol_img = symbol_img.convert("RGBA")
-	img = img.convert("RGBA")
+	img = img.rotate(180)
+	draw = ImageDraw.Draw(img)	
+	draw.text((20, 20),nb_str[0],(0,0,0),font=myBigFont)
+	draw.text((20 + BigFontSize/2 +5 , 20),nb_str[1],(0,0,0),font=myFont)
+	img = add_classic_symbol(classic_nb=classic_nb,img=img,classic_pos=classic_pos,color = "trefle",font = classicFont,symbol_size=symbol_size)
 
-	img.alpha_composite(symbol_img, dest=(60, classic_pos))
+	img = img.rotate(180)
 
 	img.save('carte_test2.png')
 	print("Card Created")
